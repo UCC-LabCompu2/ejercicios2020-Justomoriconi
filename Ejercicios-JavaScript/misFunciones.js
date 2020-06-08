5875863778237b715f61def8ba4732499b5b2edb
justomoriconi <justomoriconi@gmail.com>
2020-06-08 17:59:31 -0300

    Grados y Radianes

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -31,3 +31,15 @@ function cambiarunidades(id, valor) {
         document.unidades.unid_metro.value=0.914*valor;
     }
 }
+function convertirGR(id) {
+    var grad, rad;
+    if(id=="grados"){
+        grad = document.getElementById("grados").value;
+        rad = (grad*Math.PI)/180;
+    }else if(id=="radianes"){
+        rad = document.getElementById("radianes").value;
+        grad =(rad*180)/Math.PI;
+    }
+    document.getElementById("grados").value = grad;
+    document.getElementById("radianes").value = rad;
+}
